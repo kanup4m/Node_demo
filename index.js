5a7290405027e13a9b4fcd097819ed93e5704eb1
kanup4m <kanupam500@gmail.com>
2020-04-27 18:24:21 +0530

    Added a Procfile.

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const route = require('./Routes/routes')
 const parser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
-mongoose.connect('mongodb://localhost/create', { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = 'mongodb+srv://michael:michael123@cluster0-oipe7.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000
+}).catch(err => console.log(err.reason));
+// mongoose.connect('mongodb + srv://michael:1U8LdUzztkU5v5sq@cluster0-oipe7.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = global.Promise
 app.use(parser.json())
 app.use(route)
